a551c359071b8e418629d52cee3c440b3df712eb
Kenzo Mateus <kenzomateus44@gmail.com>
2023-09-12 22:41:03 -0300

    Project created

---
+++ ProjetoWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
