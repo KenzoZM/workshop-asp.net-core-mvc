48846c77523b0fed219fd65a46dc5619d4d98723
Kenzo Mateus <kenzomateus44@gmail.com>
2023-09-20 09:49:30 -0300

    Exception handling for delete (Referential integrity)

---
+++ ProjetoWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoWebMvc.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
